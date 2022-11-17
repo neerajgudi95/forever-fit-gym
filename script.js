b09566862530def0bafdb256c1818695e314d2dc
neerajgudi95 <neeraj.gudi@outlook.com>
2022-11-17 14:04:05 +0530

    code and structure changes

--- script.js
+++ script.js
@@ -4,3 +4,26 @@ const navUl = document.getElementById("nav-ul");
 menuBtn.addEventListener("click", () => {
   navUl.classList.toggle("show");
 });
+
+const trainers = document.querySelectorAll(".trainer");
+
+trainers.forEach((trainer, id) => {
+  let currentDelay = 300;
+  trainer.style.animationDelay = `${currentDelay + id * 50}ms`;
+});
+
+window.onload = function () {
+  trainers.forEach((trainer) => {
+    if (trainer.offsetTop < window.innerHeight) {
+      trainer.style.animationPlayState = "running";
+    }
+  });
+};
+
+window.addEventListener("scroll", function () {
+  trainers.forEach((trainer) => {
+    if (trainer.offsetTop - trainer.offsetTop / 2 < window.scrollY) {
+      trainer.style.animationPlayState = "running";
+    }
+  });
+});
